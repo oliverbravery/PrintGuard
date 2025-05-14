6dea257432a418bec1ef0bf74d373a1516270def
oliverbravery <dev@oliverbravery.uk>
2025-05-14 19:38:09 +0100

    Removed redundant import

--- fdm_sentinel/app.py
+++ fdm_sentinel/app.py
@@ -3,7 +3,6 @@ from fastapi import (
     FastAPI, Request
 )
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
