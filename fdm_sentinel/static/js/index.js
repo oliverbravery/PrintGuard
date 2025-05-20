f983fc3701d56bfe67608268ac9009cd5459eac0
oliverbravery <dev@oliverbravery.uk>
2025-05-20 12:52:47 +0100

    Added ability to render 3d ascii art in index page

--- fdm_sentinel/static/js/index.js
+++ fdm_sentinel/static/js/index.js
@@ -18,6 +18,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const totalDetectionsDisplay = document.getElementById('totalDetectionsDisplay');
     const averageFPSDisplay = document.getElementById('averageFPSDisplay');
 
+    const asciiTitle = document.getElementById('ascii-title');
+
+    function render_ascii_title(doc_element, text) {
+        figlet.defaults({ fontPath: '/static/fonts/' });
+        figlet.text(text, {
+            font: 'Big Money-ne',
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
+        }, function(err, data) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            doc_element.textContent = data;
+        });
+    }
+
+    render_ascii_title(asciiTitle, 'FDM Sentinel');
+
     function updateElapsedTime(start_time, doc_element) {
         if (!start_time) {
             doc_element.textContent = '-';
