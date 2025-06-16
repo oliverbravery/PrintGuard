e41216b31118712858d955015b3338e483e44db9
oliverbravery <dev@oliverbravery.uk>
2025-06-16 21:17:30 +0100

    Stop and start polling endpoints now require camera index in the body.

--- fdm_sentinel/routes/sse_routes.py
+++ fdm_sentinel/routes/sse_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Body
 from sse_starlette.sse import EventSourceResponse
 from ..utils.sse_utils import outbound_packet_fetch
 from ..utils.camera_utils import start_printer_state_polling, stop_and_remove_polling_task
@@ -15,11 +15,11 @@ async def sse_connect(request: Request):
     return EventSourceResponse(send_packet())
 
 @router.post("/sse/start-polling")
-async def start_polling(request: Request, camera_index: int):
+async def start_polling(request: Request, camera_index: int = Body(..., embed=True)):
     await start_printer_state_polling(camera_index)
     return {"message": "Polling started for camera index {}".format(camera_index)}
 
 @router.post("/sse/stop-polling")
-async def stop_polling(request: Request, camera_index: int):
+async def stop_polling(request: Request, camera_index: int = Body(..., embed=True)):
     stop_and_remove_polling_task(camera_index)
     return {"message": "Polling stopped for camera index {}".format(camera_index)}
